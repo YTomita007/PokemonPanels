ec9f8fb03999513cd8c3e6e3b024e747fc703b4a
Y.Tomita <y.tomita007@gmail.com>
2022-11-29 11:02:25 +0900

    resetボタンの修正、および初期アニメーションの追加

--- PokemonPanels.java
+++ PokemonPanels.java
@@ -13,38 +13,41 @@ import javax.swing.*;
 
 public class PokemonPanels extends JFrame implements ActionListener{
 
-    final int easy[] = {8, 50};     // 難易度別{1列のセルの数、セル一辺の長さ}
-    final int normal[] = {16, 30};
-    final int hard[] = {24, 20};
+    final int easy[] = {8, 50};                 // 難易度別{1列のセルの数、セル一辺の長さ}
+    final int normal[] = {16, 45};
+    final int hard[] = {24, 45};
 
-    int btn;                        // セル番号
-    int key;                        // 押されたキー情報
-    int i, j, k;                    // カウンタ変数
-    int level[] = new int[3];       // レベル設定
-    int cell;                       // 1列のセルの数
-    int cells;                      // ボード全てのセルの数
-    int side;                       // セル一辺の長さ
-    String previousCmd;             // 前のコマンド記憶
-    int previousBtn;                // １ターン前のボタン
-    int previousPoke;               // １ターン前のポケモン
-    boolean turnFlg;                // ターン制御フラグ
-    ArrayList<String> pokemonsCmd;  // ポケモンコマンド配列
-    ArrayList<String> pokemons1;    // ポケモンコマンド配列1
-    ArrayList<String> pokemons2;    // ポケモンコマンド配列2
-    HashMap<String, ImageIcon> pokemonIcons; // ポケモンアイコンハッシュマップ
-    ImageIcon icons[];              // ポケモン絵型配列
-    JButton button[];               // ボードの生成
-    JButton rButton;                // リセットボタン
-    JLabel countLabel;              // ラベルの生成
-    String cmd;                     // ActionCommandを格納する変数
-    JPanel panel = new JPanel();    // パネルを利用;
-    Timer timer;                    // 遅延変数
-    TimerTask task;                 // タイマータスク
-    Random rand = new Random();     // 乱数生成
+    static int inputNum = 2;                    // レベル選択変数
+    int btn;                                    // セル番号
+    int key;                                    // 押されたキー情報
+    int i, j, k;                                // カウンタ変数
+    int level[] = new int[3];                   // レベル設定
+    int cell;                                   // 1列のセルの数
+    int cells;                                  // ボード全てのセルの数
+    int side;                                   // セル一辺の長さ
+    int pairCount;                              // ペアカウント
+    String previousCmd;                         // 前のコマンド記憶
+    int previousBtn;                            // １ターン前のボタン
+    int previousPoke;                           // １ターン前のポケモン
+    boolean turnFlg;                            // ターン制御フラグ
+    ArrayList<String> pokemonsCmd;              // ポケモンコマンド配列
+    ArrayList<String> pokemons1;                // ポケモンコマンド配列1
+    ArrayList<String> pokemons2;                // ポケモンコマンド配列2
+    HashMap<String, ImageIcon> pokemonIcons;    // ポケモンアイコンハッシュマップ
+    ImageIcon icons[];                          // ポケモン絵型配列
+    JButton button[];                           // ボードの生成
+    JButton rButton;                            // リセットボタン
+    JLabel countLabel;                          // ラベルの生成
+    String cmd;                                 // ActionCommandを格納する変数
+    JPanel panel = new JPanel();                // パネルを利用;
+    Timer timer = new Timer(false);   // 遅延変数
+    TimerTask initTask;                         // 初期アニメーションタスク
+    TimerTask allCoverTask;                     // 全非表示タスク
+    TimerTask coverTask;                        // 非表示タスク
+    Random rand = new Random();                 // 乱数生成
 
     // 開発用に作成（実際のプレーではインスタンス化して使用するのでここから起動しない）
     public static void main(String[] args) {
-        int inputNum = 1;
         PokemonPanels frame = new PokemonPanels("PokemonPanels", inputNum);
         frame.setVisible(true);
     }
@@ -67,7 +70,7 @@ public class PokemonPanels extends JFrame implements ActionListener{
         cells = cell * cell;            // ボード全てのセルの数
         side = level[1];                // セル一辺の長さ
         button = new JButton[cells];    // ボードの生成
-        countLabel = new JLabel("ペアになった数");
+        countLabel = new JLabel("ペアになった数：" + 0);
         previousCmd = "";               // 前コマンドの初期化
     
         setTitle(title);
@@ -91,6 +94,8 @@ public class PokemonPanels extends JFrame implements ActionListener{
         rButton.setActionCommand("reset");
         rButton.setText("reset");
         panel.add(rButton);
+
+        pairCount = 0;
 
         pokemons1 = new ArrayList<String>();   // ポケモン配置するために必要
         pokemons2 = new ArrayList<String>();   // ポケモン配置するために必要
@@ -130,6 +135,25 @@ public class PokemonPanels extends JFrame implements ActionListener{
                 // button[btn].setText(pokemons.get(btn));
             }
         }
+
+		initTask = new TimerTask() {
+			@Override
+			public void run() {
+                animation();
+			}
+		};
+		timer.schedule(initTask, 1000);
+    }
+
+    public void animation(){
+        for(i=0; i<pokemonsCmd.size(); i++){
+            cmd = button[i].getActionCommand();
+            int indexK = cmd.indexOf("k");
+            int actNum = Integer.parseInt(cmd.substring(1, indexK));
+            int btnNum = Integer.parseInt(cmd.substring(indexK + 1));
+            faceUpButton(btnNum, actNum);
+        }
+        delayMethod(1500);
     }
 
     public void faceUpButton(int btnNum, int actNum){
@@ -143,16 +167,29 @@ public class PokemonPanels extends JFrame implements ActionListener{
         button[btnNum].setIcon(null);
     }
 
+    public void delayMethod(int mmseconds){
+		TimerTask allCoverTask = new TimerTask() {
+			@Override
+			public void run() {
+                for(i=0; i<cell; i++){
+                    for(j=0;j<cell;j++){
+                        btn = i * cell + j;
+                        faceDownButton(btn, btn);
+                    }
+                }
+			}
+		};
+		timer.schedule(allCoverTask, mmseconds);
+    }
+
     public void delayMethod(int btnNum, int previousBtn, int mmseconds){
-        timer = new Timer(false);
-		task = new TimerTask() {
+		coverTask = new TimerTask() {
 			@Override
 			public void run() {
                 faceDownButton(btnNum, previousBtn);
-				timer.cancel();
 			}
 		};
-		timer.schedule(task, mmseconds);
+		timer.schedule(coverTask, mmseconds);
     }
 
     @Override
@@ -160,9 +197,15 @@ public class PokemonPanels extends JFrame implements ActionListener{
         cmd = e.getActionCommand();
         // System.out.println("cmd = " + cmd);
 
-        int index = cmd.indexOf("k");
-        int actNum = Integer.parseInt(cmd.substring(1, index));
-        int btnNum = Integer.parseInt(cmd.substring(index + 1));
+        if(cmd.equals("reset")){
+            new PokemonPanels("PokemonPanels", inputNum);
+            this.setVisible(false);
+            return;
+        }
+
+        int indexK = cmd.indexOf("k");
+        int actNum = Integer.parseInt(cmd.substring(1, indexK));
+        int btnNum = Integer.parseInt(cmd.substring(indexK + 1));
 
         if(!previousCmd.equals(cmd)){
             for(String pokeKey: pokemonIcons.keySet()){ 
@@ -173,6 +216,8 @@ public class PokemonPanels extends JFrame implements ActionListener{
                         button[btnNum].setEnabled(false);
                         button[previousBtn].setBackground(Color.GRAY);
                         button[btnNum].setBackground(Color.GRAY);
+                        pairCount++;
+                        countLabel.setText("ペアになった数：" + pairCount);
                     }else{
                         delayMethod(btnNum, previousBtn, 300);
                     }
